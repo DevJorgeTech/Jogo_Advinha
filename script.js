a683c4ef6a6404313914c0622620296103ec94ae
Jorge Wagner <128604408+DevJorgeTech@users.noreply.github.com>
2023-10-31 21:02:45 -0300

    Script com funções novas
    Add:
    
    Impossível de iniciar o jogo sem escolher um dificuldade;
    Melhorias no código;
    Função de reiniciar a página;

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
 const btnCriarJogo = document.getElementById("btnCriarJogo")
 const btnEnviarPalpite = document.getElementById("btnEnviarPalpite")
 const entrada = document.getElementById("entrada")
-const numAleatorio = Math.floor(Math.random() * 100) + 1
-let maxTentativa = 0
+const palpites = document.getElementById("palpites")
+const numAleatorio = Math.floor(Math.random() * 100) + 1 // Gera um número aleátorio de 0 a 100
 
-btnCriarJogo.addEventListener("click", function verificaPalpite() {
-    let dificuldadeD = document.getElementsByName("dificuldade")
+btnCriarJogo.addEventListener("click", function verificaPalpite() { // Escolha da dificuldade    
+let dificuldadeD = document.getElementsByName("dificuldade")
 
     if (dificuldadeD[0].checked) {
         alert("Modo fácil")
         alert("Chute um número!")
-        maxTentativa = 10
+        maxTentativa = 9
 
     } else if (dificuldadeD[1].checked) {
-        maxTentativa = 7
+        maxTentativa = 6
         alert("Modo médio")
         alert("Chute um número!")
 
-    } else {
-        maxTentativa = 5
+    } else if (dificuldadeD[2].checked){
+        maxTentativa = 4
         alert("Modo díficil")
         alert("Chute um número!")
+    } else {
+        alert("Escolha um modo de jogo")
+        window.location.reload() // reinicia a página
     }
 })
 
+
 btnEnviarPalpite.addEventListener("click", function () {
-    if (entrada.value == numAleatorio) {
-        alert("Parabéns você ganhou!")
+    if (maxTentativa == 0) {
+        alert("Perdeu")
         novoJogo()
-    } else if (entrada.value < numAleatorio) {
-        alert("Chute um valor maior!")
-        maxTentativa = maxTentativa- 1
-        if (maxTentativa == 0) {
-            alert("Perdeu!")
-            novoJogo()
-        }
+        window.location.reload() // reinicia a página
+    } else if (maxTentativa == 1){
+        alert("ÚLTIMA TENTATIVA!")
+        maxTentativa--
     } else {
-        alert("Chute um valor menor")
-        maxTentativa = maxTentativa- 1
-        if (maxTentativa == 0) {
-            alert("Perdeu!")
+        if (entrada.value == numAleatorio) {
+            alert("Parabéns você ganhou!")
             novoJogo()
+            window.location.reload() // reinicia a página
+        } else if (entrada.value < numAleatorio) {
+            alert("Chute um valor maior!")
+            maxTentativa--
+        } else {
+            alert("Chute um valor menor")
+            maxTentativa--
         }
     }
+
+    palpitesLista = []
+    palpitesLista.push(entrada.value)
+    palpites.innerHTML += palpitesLista + " "
+
+
 })
 
 function novoJogo() {
